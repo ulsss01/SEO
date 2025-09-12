e40ee534dac256d93d0d236bc94c45060543888e
jin <nsj9905@naver.com>
2025-09-12 18:56:58 +0900

    new

---
+++ src/test/java/kr/co/dong2/DataSourceTest.java
@@ -0,0 +1,31 @@
+package kr.co.dong2;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/*.xml"})
+public class DataSourceTest {
+	@Autowired
+	private DataSource dataSource;
+	
+	@Test
+	public void testConn() {
+		try {
+			Connection conn = dataSource.getConnection();
+			System.out.println(conn);
+			System.out.println("연결성공 ~");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
